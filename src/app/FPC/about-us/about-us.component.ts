f22e55928eaa69858799e8b957e6274c1c7b3ea3
vamsee <vamsee164@gmail.com>
2025-06-29 20:09:48 +0530

    Ui Changes

--- src/app/FPC/about-us/about-us.component.ts
+++ src/app/FPC/about-us/about-us.component.ts
@@ -12,35 +12,11 @@ export class AboutUsComponent {
   // Array to hold team member data
   teamMembers = [
     {
-      name: 'Milind Deshpande',
-      role: 'CEO',
-      experience: 'Experience 30+ Years',
+      name: 'D Uday Kumar',
+      role: 'Founder',
       imageUrl: 'https://placehold.co/150x150/1a2a4b/ffffff?text=CEO', // Dummy image
     },
-    {
-      name: 'Uttam Mane',
-      role: 'Ex.CGMT MESSOL, Advisor IT',
-      experience: 'Experience 30+ Years',
-      imageUrl: 'https://placehold.co/150x150/1a2a4b/ffffff?text=UTM', // Dummy image
-    },
-    {
-      name: 'Prashant Mathur',
-      role: 'Chief Impact Officer',
-      experience: 'Experience 20+ Years',
-      imageUrl: 'https://placehold.co/150x150/1a2a4b/ffffff?text=PM', // Dummy image
-    },
-    {
-      name: 'Manoj Hate',
-      role: 'Advisor Public Relations',
-      experience: 'Experience 20+ Years',
-      imageUrl: 'https://placehold.co/150x150/1a2a4b/ffffff?text=MH', // Dummy image
-    },
-    {
-      name: 'KV Kurundkar, IAS (Retd.)',
-      role: 'Advisor Govt Business',
-      experience: 'Experience 33+ Years',
-      imageUrl: 'https://placehold.co/150x150/1a2a4b/ffffff?text=KV+K', // Dummy image
-    },
+   
   ];
 
   constructor() {}
